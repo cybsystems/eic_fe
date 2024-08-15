9840a49d7a83af6189322fba676f55bd110d40bd
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-16 00:37:26 +0530

    autoClose time set

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 export const showToast=(type:"sucess"|"error",message:string)=>{
     const toastMethod=type==="error"?toast.error:toast.success;
     toastMethod(message,{
-        position: "bottom-center"
+        position: "bottom-center",
+        autoClose:2000
       });
 }
