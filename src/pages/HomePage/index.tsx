ce9dd06e6918161e10120946eb370f904dc1d741
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-16 01:12:05 +0530

    minor changes

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ModuleContainer from "@components/atoms/ModuleContainer";
 import AppBar from "@components/molecules/AppBar";
 import DrawerComponent from "@components/molecules/AppDrawer";
@@ -5,16 +6,21 @@ import { useAuth } from "@contexts/authContext";
 import useDeviceType from "@hooks/useMediaDevice";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import * as React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const { isMobile } = useDeviceType();
 
-  const [open, setOpen] = React.useState(!isMobile); // Drawer open state
+  const [open, setOpen] = useState(!isMobile); // Drawer open state
 
   const { logout } = useAuth();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isMobile) {
+      setOpen(false);
+    }
+  }, [isMobile]);
 
   const handleDrawerOpen = () => {
     if (!isMobile) setOpen(true);
