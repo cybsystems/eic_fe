de53a1086d1570ab270ae8d5ee84ddd1f5f43303
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2024-08-27 01:07:18 +0530

    reports added

---
+++ src/pages/ReportsPage/helper.tsx
@@ -0,0 +1,121 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import apiInstance from "@api/index";
+import { Box } from "@mui/material";
+
+export const DISPATCHES_COLUMNS = [
+  {
+    field: "id",
+    headerName: "ID",
+    width: 90,
+  },
+  {
+    field: "itemName",
+    headerName: "Item",
+    flex: 1,
+  },
+  {
+    field: "quantity",
+    headerName: "Quantity",
+    flex: 1,
+  },
+  {
+    field: "projectName",
+    headerName: "Project",
+    flex: 1,
+  },
+  {
+    field: "unitName",
+    headerName: "Unit",
+
+    flex: 1,
+  },
+  {
+    field: "contractorName",
+    headerName: "Contractor",
+
+    flex: 1,
+  },
+];
+
+export const STOCKS_COULMNS = [
+  {
+    field: "id",
+    headerName: "ID",
+    width: 90,
+    align: "center",
+  },
+  {
+    field: "item",
+    headerName: "Name",
+    flex: 1,
+  },
+  {
+    field: "category",
+    headerName: "Category",
+    renderCell: (params: any) => params.row.category.name,
+    flex: 1,
+  },
+  {
+    field: "feature",
+    headerName: "Feature",
+    renderCell: (params: any) => params.row.feature.feature,
+    flex: 1,
+  },
+  {
+    field: "quantity",
+    headerName: "Quantity",
+    renderCell: (params: any) => <Box>{params.value}</Box>,
+    flex: 1,
+  },
+];
+export const INWARD_COLUMNS = [
+  {
+    field: "id",
+    headerName: "ID",
+    width: 90,
+  },
+  {
+    field: "itemName",
+    headerName: "Item",
+    flex: 1,
+  },
+  {
+    field: "quantity",
+    headerName: "Quantity",
+    flex: 1,
+  },
+  {
+    field: "warehouseName",
+    headerName: "Warehouse",
+    flex: 1,
+    renderCell: (params: any) => <Box>{params.value || "-"}</Box>,
+  },
+  {
+    field: "contractorName",
+    headerName: "Contractor",
+    renderCell: (params: any) => <Box>{params.value || "-"}</Box>,
+    flex: 1,
+  },
+  {
+    field: "vendorName",
+    headerName: "Vendor",
+
+    flex: 1,
+    renderCell: (params: any) => <Box>{params.value || "-"}</Box>,
+  },
+];
+
+export const getInwardReports = async () => {
+  const res = await apiInstance.get("reports/inward");
+  return res;
+};
+
+export const getStockReports = async () => {
+  const res = await apiInstance.get("reports/stock");
+  return res;
+};
+
+export const getDispatchReports = async () => {
+  const res = await apiInstance.get("reports/dispatch");
+  return res;
+};
--- src/pages/ReportsPage/index.tsx
+++ src/pages/ReportsPage/index.tsx
@@ -1,19 +1,121 @@
-import Breadcrumbs from '@components/atoms/Breadcrumbs'
-import PageGridContainer from '@components/atoms/PageGridContainer'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
+import {
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  Grid,
+  Paper,
+  Stack,
+} from "@mui/material";
+import Breadcrumbs from "@components/atoms/Breadcrumbs";
+import PageGridContainer from "@components/atoms/PageGridContainer";
+import {
+  DISPATCHES_COLUMNS,
+  getDispatchReports,
+  getInwardReports,
+  getStockReports,
+  INWARD_COLUMNS,
+  STOCKS_COULMNS,
+} from "./helper";
+import { formatError, showToast } from "@utils/index";
+import DataTable from "@components/atoms/DataTable";
+
+const reportTypes: any = {
+  inward: {
+    id: "inward",
+    title: "Inward",
+    controller: getInwardReports,
+    cols: INWARD_COLUMNS,
+  },
+  dispatch: {
+    id: "dispatch",
+    title: "Dispatch",
+    controller: getDispatchReports,
+
+    cols: DISPATCHES_COLUMNS,
+  },
+  stock: {
+    id: "stock",
+    title: "Stock",
+    controller: getStockReports,
+    cols: STOCKS_COULMNS,
+  },
+};
 
 const ReportsPage = () => {
+  const [selectedReportType, setSelectedReportType] = useState("");
+  const [cols, setCols] = useState([]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchReportData = async (reportType: string) => {
+    try {
+      const reportMap = reportTypes[reportType];
+      if (reportMap.controller) {
+        const reportData = await reportMap.controller();
+        return reportData;
+      }
+      return [];
+    } catch (error) {
+      showToast("error", formatError(error));
+      return [];
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleChange = async (event: any) => {
+    const reportType = event.target.value;
+    const reportMap = reportTypes[reportType];
+    setData([]);
+    setCols(reportMap.cols);
+
+    setLoading(true);
+    setSelectedReportType(reportType);
+
+    const reportData = await fetchReportData(reportType);
+    setData(reportData.data);
+  };
+
   return (
     <PageGridContainer>
-              <Breadcrumbs
+      <Breadcrumbs
         breadcrumbs={[
           { label: "Home", href: "/" },
           { label: "Reports", href: "/reports" },
         ]}
         title="Reports"
       />
+      <Grid item xs={12}>
+        <Paper elevation={3} sx={{ padding: 4 }}>
+          <FormControl fullWidth margin="normal">
+            <InputLabel id="report-type-label">Report Type</InputLabel>
+            <Select
+              labelId="report-type-label"
+              value={selectedReportType}
+              onChange={handleChange}
+              label="Report Type"
+            >
+              {Object.values(reportTypes).map((type: any) => (
+                <MenuItem key={type.id} value={type.id}>
+                  {type.title}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Paper>
 
+        {selectedReportType && (
+          <Stack marginTop={5}>
+            <Paper>
+              <DataTable loading={loading} columns={cols} rows={data} />
+            </Paper>
+          </Stack>
+        )}
+      </Grid>
     </PageGridContainer>
-  )
-}
+  );
+};
 
-export default ReportsPage
+export default ReportsPage;
